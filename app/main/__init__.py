e6e4fd353366177c8dfab6957d6f7f9761e2d925
Sam Firke <samuel.firke@gmail.com>
2026-05-10 17:14:03 -0400

    avoid circular dependency

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,13 @@
 from flask import Blueprint
 
-from app.main.views import browse, giveaways, items, loans, messaging, profile, public  # noqa: F401
-
 bp = Blueprint("main", __name__)
+
+from app.main.views import (  # noqa: F401, E402
+    browse,
+    giveaways,
+    items,
+    loans,
+    messaging,
+    profile,
+    public,
+)
